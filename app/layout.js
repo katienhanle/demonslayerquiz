e6358ea1a5aeffbaf0cc659e5ee1c8a7065b9d4d
kal070 <kal070@ucsd.edu>
2025-10-18 21:36:31 -0700

    Vercel Analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 // app/layout.js
 import "./globals.css";
 import { Jersey_25 } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
+
 
 const jersey25 = Jersey_25({ subsets: ["latin"], weight: "400" });
 
@@ -9,7 +11,7 @@ export const metadata = { title: "Corps Exam", description: "Demon Slayer Inspir
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jersey25.className}>{children}</body>
+      <body className={jersey25.className}>{children}<Analytics /></body>
     </html>
   );
 }
